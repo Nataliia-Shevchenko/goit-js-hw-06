16097be74c6da9462e819521d4736be886493242
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-02-26 17:50:34 +0200

    update task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const rangeEl = document.querySelector('#font-size-control');
 
 const textEl = document.querySelector('#text');
+textEl.style.fontSize = `${rangeEl.value}px`;
 
 rangeEl.addEventListener('input', onRange);
 
